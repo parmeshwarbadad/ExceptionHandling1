0272d889610aad98dd912357f0c985bb0c7066e8
Sunil Borate <Sunil Borate@DESKTOP-0VTO70S>
2022-07-18 16:31:44 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling1
+{
+    class Class1
+    {
+        public static void m1(int a,int b)
+        {
+            Console.WriteLine("m1 starts");
+            Console.WriteLine("Division "+(a/b));
+            Console.WriteLine("main ends");
+        }
+        public static void m2(int x, int y)
+        {
+            Console.WriteLine("in m2 start");
+            m1(x, y);
+            Console.WriteLine("in m2 ends");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main start");
+            try
+            {
+                m2(10, 0);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message );
+            }
+            Console.WriteLine("main ends");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExceptionHandling1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter two num");
+                int n1 = int.Parse (Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Division "+(n1/n2));
+                Console.WriteLine("try ends");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("In a catch 2");
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("in a catch 1");
+                Console.WriteLine(e.Message);
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine("In a catch 3");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
